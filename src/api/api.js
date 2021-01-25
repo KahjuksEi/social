e62a16e363759f4092db73f6798e5768021ada51
unknown <andrew.shakalov@gmail.com>
2021-01-25 21:00:58 +0300

    63

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+import * as axios from "axios";
+
+/*делаем инстанс чтоб не дублироваться*/
+const instance = axios.create({
+  withCredentials: true,
+  /*baseURL: "https://social-network.samuraijs.com/api/1.0/",*/
+  headers: {
+    "API-KEY": "9d339b31-96cf-4a17-b83a-20f7aa16f051",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(
+        /*baseURL + */ `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+        {}
+      )
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
