fcf2d62883be354662dadecbecc293cbe2d4de3d
unknown <andrew.shakalov@gmail.com>
2021-03-21 11:54:22 +0300

    80

--- src/App.js
+++ src/App.js
@@ -5,25 +5,46 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import Friend from "./components/Friend/Friend";
-import { Route } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import Login from "./components/Login/Login";
 import "./App.css";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { initializeApp } from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
-function App(props) {
-  return (
-    <div className="App-wrapper">
-      <HeaderContainer />
-      <Navigation />
-      {/* показываем что у урла могут быть параметры, "?" дает необяз-сть параметра */}
-      <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-      <Route path="/dialogs" render={() => <DialogsContainer />} />
-      <Route path="/users" render={() => <UsersContainer />} />
+class App extends React.Component {
+  componentDidMount() {
+    this.props.initializeApp();
+  }
+  render() {
+    if (!this.props.initialized) {
+      return <Preloader />;
+    }
 
-      <Route path="/login" render={() => <Login />} />
-      <p>Friends online:</p>
-      {/* <Friend state={props.state.dialogsPage} /> */}
-    </div>
-  );
+    return (
+      <div className="App-wrapper">
+        <HeaderContainer />
+        <Navigation />
+        {/* показываем что у урла могут быть параметры, "?" дает необяз-сть параметра */}
+        <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+        <Route path="/dialogs" render={() => <DialogsContainer />} />
+        <Route path="/users" render={() => <UsersContainer />} />
+
+        <Route path="/login" render={() => <Login />} />
+        <p>Friends online:</p>
+        {/* <Friend state={props.state.dialogsPage} /> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized,
+});
+
+/*export default App;*/
+export default compose(
+  withRouter,
+  connect(mapStateToProps, { initializeApp })
+)(App);
